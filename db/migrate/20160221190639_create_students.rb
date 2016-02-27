b866c77dbf0df9691cd56918f1a6ef5974112939
P. Ross Baldwin <ross@rossbaldwin.com>
2016-02-27 02:29:57 +0000

    added last name to students and propagated in views and controller

--- db/migrate/20160221190639_create_students.rb
+++ db/migrate/20160221190639_create_students.rb
@@ -2,6 +2,7 @@ class CreateStudents < ActiveRecord::Migration
   def change
     create_table :students do |t|
       t.string :name
+      t.string :last_name
 
       t.timestamps null: false
     end
--- db/schema.rb
+++ db/schema.rb
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 20160221190926) do
 
   create_table "students", force: :cascade do |t|
     t.string   "name"
+    t.string   "last_name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
