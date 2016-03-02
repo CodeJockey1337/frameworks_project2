a128861aeea4439f1711bba99653d5533f9d956e
P. Ross Baldwin <ross@rossbaldwin.com>
2016-03-02 01:40:17 +0000

    Starting authentication

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,8 +3,10 @@ class Course < ActiveRecord::Base
     
     #validations - never assume your forms are used!
     validates :name, presence: true, length: { minimum: 2, maximum: 30 }
+    validates_format_of :name, with: /\A[A-Za-z 0-9]+\z/, message: 'must contain only numbers, letters and spaces'
     validates :course_code, presence: true, length: { is: 6 }
     validates :department_name, presence: true, length: { is: 2 }
+    validates_format_of :department_name, with: /\A[A-Z]{2}\z/, message: 'must be 2 capital letters'
     
     
     validates_uniqueness_of :name
